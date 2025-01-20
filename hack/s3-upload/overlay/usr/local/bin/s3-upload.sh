b4967a869badb108002282393fd0149bced18bf5
James Harmison <jharmison@redhat.com>
2025-01-20 12:13:27 -0500

    fix: some tweaks for input handling and output messages

--- hack/s3-upload/overlay/usr/local/bin/s3-upload.sh
+++ hack/s3-upload/overlay/usr/local/bin/s3-upload.sh
@@ -4,8 +4,13 @@ set -e
 
 # Ensure we're logged in
 if ! oc whoami >/dev/null 2>&1; then
-    echo "You don't appear to be logged in to an OpenShift cluster inside the container." >&2
+    echo "You don't appear to be logged in to an OpenShift cluster inside the container. Check the docs for prerequisites!" >&2
     exit 1
+fi
+
+if (("${#}" < 1)); then
+    echo "usage: upload.sh PATH [PATH [...]]" >&2
+    exit 0
 fi
 
 echo -n "Connecting to cluster: "
@@ -22,6 +27,7 @@ if [ "${AWS_S3_ENDPOINT}" = "http://s3.openshift-storage.svc" ]; then
     export AWS_S3_ENDPOINT="https://$(oc get route.route -n openshift-storage s3 -ojsonpath='{.status.ingress[0].host}')"
 fi
 
+# Create some useful variables for s3cfg
 scheme=$(echo "$AWS_S3_ENDPOINT" | cut -d: -f1)
 s3_host=$(echo "$AWS_S3_ENDPOINT" | cut -d/ -f3-)
 if [ "$scheme" = "http" ]; then
@@ -46,20 +52,15 @@ use_https = ${use_https}
 EOF
 
 function s3cmd {
+    set -x
     /usr/local/bin/s3cmd -c /tmp/s3cfg "${@}"
+    { set +x; } 2>/dev/null
 }
 dest="s3://${AWS_S3_BUCKET}/models/"
 
+# Upload directories recursively, using
 for input in "${@}"; do
-    if [ -d "$input" ]; then
-        set -x
-        s3cmd put --recursive "${input%/}/" "$dest"
-        { set +x; } 2>/dev/null
-    else
-        set -x
-        s3cmd put "${input}" "$dest"
-        { set +x; } 2>/dev/null
-    fi
+    s3cmd put --recursive "$input" "$dest"
 done
 
 set -x
